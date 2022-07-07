79ef97fddabe5ad0684ad1acca71e3150860d52a
Luke G <lukejgranered@gmail.com>
2022-07-07 16:29:15 -0500

    digit button class added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './styles.css';
 import React from 'react';
 import { useReducer } from 'react';
+import DigitButton from './DigitButton';
 
-function reducer(state, action) {
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
 
+function reducer(state, { type, payload }) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ''}${payload.digit}`
+      }
+  }
 }
 
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+
   return (
      <div className='calculator-grid'>
         <div className='output'>
@@ -20,22 +36,22 @@ function App() {
         </div>
         <button className='span-two'>AC</button>
         <button>DEL</button>
-        <button> ÷ </button>
-        <button> 1 </button>
-        <button> 2 </button>
-        <button> 3 </button>
-        <button> * </button>
-        <button> 4 </button>
-        <button> 5 </button>
-        <button> 6 </button>
-        <button> + </button>
-        <button> 7 </button>
-        <button> 8 </button>
-        <button> 9 </button>
-        <button> - </button>
-        <button> . </button>
-        <button> 0 </button>
-        <button className='span-two'> = </button>
+        {/* <DigitButton digit='÷' dispatch={dispatch} /> */}
+        <DigitButton digit='1' dispatch={dispatch} />
+        <DigitButton digit='2' dispatch={dispatch} />
+        <DigitButton digit='3' dispatch={dispatch} />
+        <button>*</button>
+        <DigitButton digit='4' dispatch={dispatch} />
+        <DigitButton digit='5' dispatch={dispatch} />
+        <DigitButton digit='6' dispatch={dispatch} />
+        <button>+</button>
+        <DigitButton digit='7' dispatch={dispatch} />
+        <DigitButton digit='8' dispatch={dispatch} />
+        <DigitButton digit='9' dispatch={dispatch} />
+        <button>-</button>
+        <DigitButton digit='.' dispatch={dispatch} />
+        <DigitButton digit='0' dispatch={dispatch} />
+        <button className='span-two'>=</button>
      </div>
   );
 }
